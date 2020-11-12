cd4d7ccb4a11f551e00f8f5d3a6cfc787cf48be7
Akram2000 <akramhahs@gmail.com>
2020-11-12 00:56:51 +0000

    Added Winzone

---
+++ Caught/Assets/Scripts/WinCollider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCollider : MonoBehaviour
+{
+    Boolean winText = false;
+    private GUIStyle guiStyle = new GUIStyle();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Escapee has reached the win zone");
+        winText = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        winText = false;
+    }
+
+    void OnGUI()
+    {
+        guiStyle.fontSize = 40;
+        if (winText)
+        {
+            GUI.Label(new Rect(15, 250, 100, 100), "An Escapee has reached the win zone!", guiStyle);
+        }
+    }
+
+
+}
